2d0494c27534031a2019c677f93b49b50f7827da
braga <pedro.reis@mds.gov.br>
2025-04-02 17:20:22 -0300

    Removing target folder and resolving some problems with workspace

--- src/test/java/com/rest/Feature/DocumentsTest.java
+++ src/test/java/com/rest/Feature/DocumentsTest.java
@@ -1,10 +1,8 @@
 package com.rest.Feature;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rest.TestConfig;
 import com.rest.endpoint.Controllers.DocumentController;
 import com.rest.entity.Document;
-import com.rest.entity.User;
 import com.rest.repository.IDocumentDAO;
 import com.rest.repository.IUserDAO;
 import com.rest.service.JwtService;
@@ -22,17 +20,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Import(TestConfig.class)
@@ -42,8 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // authentication controller.
 @WebMvcTest(DocumentController.class)
 
-@WithMockUser(username = "user", roles = { "ADMIN" })
-public class DocumentControllerTest {
+public class DocumentsTest {
 
   @Autowired
   private MockMvc mvc;
@@ -60,9 +50,8 @@ public class DocumentControllerTest {
   @MockBean
   private UserDetails userDetails;
 
-  private static final Logger LOGGER = LogManager.getLogger();
-
   @Test
+  @WithMockUser(username = "user", roles = { "ADMIN" })
   public void shouldReturnAllDocuments() throws Exception {
 
     // Mock Data
@@ -89,40 +78,5 @@ public class DocumentControllerTest {
         .andExpect(jsonPath("$[0].name").value("Document 1"))
         .andExpect(jsonPath("$[1].id").value(2))
         .andExpect(jsonPath("$[1].name").value("Document 2"));
-  }
-
-  @Test
-  public void shouldCreateDocument() throws Exception {
-
-    // Mock Data
-    Document document = new Document();
-    document.setId(1L);
-    document.setName("Document 1");
-
-    // Mock Data
-    User user = new User("root", "root@root.com", "root");
-    user.setId(1L);
-
-    ObjectMapper objectMapper = new ObjectMapper();
-
-    // Create request body
-    Map<String, Object> requestBody = new HashMap<>();
-    requestBody.put("name", document.getName());
-    requestBody.put("userId", user.getId());
-
-    // Convert to JSON
-    String jsonContent = objectMapper.writeValueAsString(requestBody);
-    System.out.println(jsonContent);
-
-    LOGGER.info("jsonContent {}", jsonContent);
-
-    // Mock repository behavior
-    when(documentRepository.findById(document.getId())).thenReturn(Optional.of(document));
-
-    // Perform GET request and verify response
-    mvc.perform(post("/api/v1/documents")
-        .content(jsonContent)
-        .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
   }
 }
